daeb044a7f6212c81f317c53f37c982f43b36cf6
mariangelan <42880624+mariangelan@users.noreply.github.com>
2021-10-12 15:24:12 -0500

    Mostrar calendario de un año

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,136 @@
 
 #create by Mariangela
+class Dia: #clase para guardar los datos de un dia
+    def __init__(self, dia, fecha):
+        self.dia = dia #numero del dia (0 Domingo, 1 Lunes, ...)
+        self.fecha = fecha
+    def diaFecha(self):
+        return str(self.fecha[2]) #String del dia
+
+class Semana: #clase para guardar los datos de una seman
+    def __init__(self):
+        self.diasSemana = []
+    def addDia(self,diaNuevo):
+        self.diasSemana.append(diaNuevo)
+    def imprimirSemana(self): #Metodo que da el formato a los dias de la semana
+        strSemana = ""   
+        if(len(self.diasSemana) < 7 and len(self.diasSemana) > 0 and  self.diasSemana[0].dia > 0): #Cuantos espacios adelante se ocupan
+            numEspacio = self.diasSemana[0].dia#Cantidadde espacios que se deja
+            for i in range(0,numEspacio):
+                strSemana += '{:>3}'.format("")
+        for dia in self.diasSemana: #dias de la semana
+                strSemana += '{:>3}'.format(str(dia.diaFecha()))
+        
+        return '{:<21}'.format(strSemana)+" |"
+
+class Mes: #clase que representa a un mes del calendario
+    def __init__(self,nombre,numeroMes):
+        self.nombre = nombre #nombre del mes
+        self.numMes = numeroMes #numero que representa ese mes
+        self.semanas = [] #Lista de sus respectivas semanas
+    def addSemana(self,semanaNueva):
+        self.semanas.append(semanaNueva)
+    def imprimirSemanaMes(self,semana):
+        strSemana = ""
+        if(semana < len(self.semanas)):
+            strSemana = self.semanas[semana].imprimirSemana()
+        elif(semana >= len(self.semanas)):
+            strSemana = '{:<21}'.format(strSemana)+" |"
+        return strSemana
+    def imprimirInfoMes(self): #Retornar un string con el formato de los días
+        return '{:>3}'.format("D")+'{:>3}'.format("L")+'{:>3}'.format("K")+'{:>3}'.format("M")+'{:>3}'.format("J")+'{:>3}'.format("V")+'{:>3}'.format("S")+" |"
+    def imprimirNombre(self): #Retorna el nombre del mes 
+        return '{:^21}'.format(str(self.nombre))+" |"
+
+class Calendario: #clase que representa a un calendario
+    def __init__(self,año):
+        self.año = año #posee un año y una lista de meses
+        self.meses = []
+    def addMes(self, mesNuevo):
+        self.meses.append(mesNuevo)
+    def imprimirCalendarioInfo(self): #Titulo del claendario
+        print('{:^87}'.format("Calendario del año "+str(self.año)))
+    def imprimirMes(self, ini, fin): #inicio debe ser menor que fin y debe de ser menor a 12 y mayo a 1
+        linea = ""                   #Se busca que se imprima los meses que desee el usuario
+        for i in range(ini, fin):
+            linea += self.meses[i].imprimirNombre() 
+        print(linea)
+        linea = ""
+        for i in range(ini, fin):
+            linea += self.meses[i].imprimirInfoMes() 
+        print(linea)
+        linea = ""
+        for j in range(0,6):
+            for i in range(ini,fin):    
+                linea += self.meses[i].imprimirSemanaMes(j)
+            print(linea)
+            linea = ""
+class CrearCalendario : 
+       
+#Crear Calendario
+#Dado un año se crea los meses y dias que este posee
+#Para esto se usan las clasesx Día, Semana, Mes
+    def crearCalendario(año):
+        nuevoCalen = Calendario(año)
+        ##Se procede a crear los meses
+        ##Se tiene una lista de los mese
+        mesesNom = ["Enero","Febrero","Marzo", "Abril", "Mayo", "Junio",
+                    "Julio", "Agosto","Septiembre", "Octubre", "Noviembre", "Diciembre"]
+        contadorDiaSemana = Validaciones.primero_enero(nuevoCalen.año)
+        for i in range(0, len(mesesNom)):
+            contadorDiaSemana = CrearCalendario.crearMes(mesesNom[i],i+1,nuevoCalen, contadorDiaSemana) #Se pasa el calendario para trabajar por refernecia
+        return nuevoCalen
+
+    #Se crea un Mes, para ser agregado al Calendario
+    def crearMes(nombreMes, numeroMes, calendario, diaSemana):
+        nuevoMes = Mes(nombreMes, numeroMes)
+        ##Se procede a crear las semanas del mes nuevo
+
+        diaSemana = CrearCalendario.crearSemanasMes(nuevoMes, calendario.año, diaSemana)
+            
+        ##Se agrega el mes al Calendario
+        calendario.addMes(nuevoMes)
+        return diaSemana #Contador de que dia sigue para el siguiente Mes
+
+    def crearSemanasMes(mesAct, año, diaSemanaAct):
+        contadorDiasMes = cantidadDiasMes(mesAct.numMes, año)
+        semana = Semana()
+        for diaActFecha in range(1, contadorDiasMes+1): #Se crean los dias de ese mes
+            #Se crea un nuevo Dia
+            nuevoDia = Dia(diaSemanaAct, (año,mesAct.numMes,diaActFecha))
+            semana.addDia(nuevoDia) #Se agrega el dia a la semana
+            if(diaSemanaAct == 6): #Si ya el dia es 6, se crea una nueva semana
+                diaSemanaAct = 0 #Se vuelve el día lunes
+                mesAct.addSemana(semana)#Se agrega la semana al mes
+                semana = Semana() #Se crea una nueva semana
+            else:
+                diaSemanaAct += 1 #Si es otro dia solo se suma el contador
+        if(diaSemanaAct <= 6):
+            mesAct.addSemana(semana)
+        return diaSemanaAct
+                
+    
+
+#Esta funcion me da el contador de dias, dependiendo del mes y año dado
+def cantidadDiasMes(numMes, año):
+    meses31 = {1,3,5,7,8,10,12} ##Meses que poseen 31 dias
+    meses30 = {4,6,9,11} ##Meses que poseen 30 dias
+    dias = 0 
+    if(numMes in meses31):
+        dias = 31  
+    elif(numMes in meses30):
+        dias = 30
+    elif(numMes == 2): # es febrero
+        
+            bisiesto = validar.Bisiesto(año)
+            if (bisiesto == True):
+                dias = 29
+            else:
+                dias = 28
+    return dias
+    
+
+        
 class Validaciones:
     def __init__(self):
         self.fechas = []
@@ -126,11 +257,15 @@ class Validaciones:
             dia = dia % 7
             añoInicio += 1
         return dia
-
-
-
-
-
+    def primero_enero(año):
+        if type (año) != int or año < 1583:
+            return -1
+        else:
+            a = (14 - 1) // 12
+            y = año - a
+            m = 1 + 12 * a - 2
+            d = (1 + y + (y//4) - (y//100) + (y//400) + ((31 * m)//12)) % 7
+        return d
 
 
 validar = Validaciones()
@@ -245,6 +380,21 @@ def consultarBisiestos():
     elif (bisiesto == False):
         print("»No es bisiesto \n")
     return True
+
+
+def imprimir_3x4(año): ##
+    calend= CrearCalendario.crearCalendario(año) ##Se crea la estructura de un calendario
+    calend.imprimirCalendarioInfo()
+    calend.imprimirMes(0,4)
+    calend.imprimirMes(4,8)
+    calend.imprimirMes(8,12)
+
+ 
+
+
+
+
+
 estado = True
 opcion = 0
 print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░♠░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ")
@@ -258,6 +408,7 @@ while(estado):
     print("3) Día siguiente de una fecha ")
     print("4) Determinar los días que han pasado desde el primero de ese año. ")
     print("5) Dia específico del primero de enero dado un año. ")
+    print("6) Ver calnedario de un año. ")
     print("0) Salir del programa. ")
     print("\n")
     try:
@@ -283,6 +434,9 @@ while(estado):
             estadoConsulta = diaSemana()
             while (not estadoConsulta):
                 estadoConsulta = diaSemana()
+        elif (opcion == 6):
+            año = int(input("Digite el año: "))
+            imprimir_3x4(año)             
         elif (opcion == 0):
             estado = False
         else:
